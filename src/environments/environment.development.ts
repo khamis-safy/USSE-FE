8c2b2b2a232631a72cb835b5251cb8e61a8b4128
khamis safy <51087598+khamis-safy@users.noreply.github.com>
2023-08-10 20:29:27 +0300

    required configs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,4 +2,6 @@ export const environment = {
   production: true,
   domain:"https://commswift.formmozo.com/api/",
   api:"http://dev-smpp.uaenorth.cloudapp.azure.com:5300/api/"
+  //api:"http://localhost:5300/api/"
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
   domain:"https://commswift.formmozo.com/api/",
-  api:"http://dev-smpp.uaenorth.cloudapp.azure.com:5300/api/"
+  //api:"http://dev-smpp.uaenorth.cloudapp.azure.com:5300/api/"
+  api:"http://localhost:5300/api/"
 };
