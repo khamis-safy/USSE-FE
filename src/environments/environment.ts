7f81446b8d5211abec964f8c950b0f425f3c3e80
khamis safy <51087598+khamis-safy@users.noreply.github.com>
2023-08-26 13:59:56 +0300

    docker config

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const environment = {
   production: false,
   domain:"https://commswift.formmozo.com/api/",
   //api:"http://dev-smpp.uaenorth.cloudapp.azure.com:5300/api/"
-  api:"http://localhost:5300/api/"
+  //api:"http://localhost:5300/api/"
+  api:"http://commswift:5300/api/"
 };
