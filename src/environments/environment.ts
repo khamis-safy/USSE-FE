ae2f5b1fc0989d9785bb8cc4563e62c8e538b213
khamis safy <51087598+khamis-safy@users.noreply.github.com>
2023-09-14 20:59:35 +0300

    update ip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,6 +4,6 @@ export const environment = {
   //api:"http://dev-smpp.uaenorth.cloudapp.azure.com:5300/api/"
   //api:"http://localhost:5300/api/"
   //api:"http://commswift:5300/api/",
-  api:"http://commswift:5300/api/"
+  api:"http://40.123.212.178:5300/api/"
 
 };
