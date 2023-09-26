c794ece53124ac0400568ada6b25eb8d156ed5e8
Ahlam Ragab <ahlam.ragab2000@gmail.com>
2023-09-26 23:07:13 +0300

    fix

--- src/app/layouts/layout-two/layout-two.routing.ts
+++ src/app/layouts/layout-two/layout-two.routing.ts
@@ -1,7 +1,6 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LayoutTwoComponent } from './layout-two.component';
 import { AuthGuard } from 'src/app/shared/services/auth.guard';
-import { PermissionResolverService } from 'src/app/shared/services/permissionResolver.service';
 
 const routes: Routes = [
   {
@@ -55,7 +54,6 @@ const routes: Routes = [
       {
         path: "compaigns",
         canActivate:[AuthGuard],
-        // resolve: { permissions: PermissionResolverService },
 
         loadChildren: () =>
           import("./../../pages/compaigns/compaigns.module").then((m) => m.CompaignsModule),
