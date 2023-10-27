15a076105c4e4762a8831a81de206960e24e89f7
khamis safy <51087598+khamis-safy@users.noreply.github.com>
2023-10-27 16:06:58 +0200

    update version number
    update version number to v1.0.0

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   api:"https://backend.qweira.com/api/",
-  secretKey:"a1d227f8-1749-4676-b41d-0d8c7605339c"
-  
+  secretKey:"a1d227f8-1749-4676-b41d-0d8c7605339c",
+  version:"V1.0.0"
 };
