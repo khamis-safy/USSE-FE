81ff6bb3aeec2c351f4fb844430d6b99b7aeb4d0
Ahlam Ragab <ahlam.ragab2000@gmail.com>
2023-11-03 14:04:21 +0200

    sprint 1 release

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   api:"http://40.123.212.178:5300/api/",
   secretKey:"a1d227f8-1749-4676-b41d-0d8c7605339c",
-  version:"V1.0.10"
+  version:"V1.1.0"
 };
