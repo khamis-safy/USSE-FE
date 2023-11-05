428b6a2103e259445394d4e864f5467cf86c5cce
Ahlam Ragab <ahlam.ragab2000@gmail.com>
2023-11-05 20:06:47 +0200

    display-file-type-img in messages

--- src/app/shared/pipes/deviceType.pipe.ts
+++ src/app/shared/pipes/deviceType.pipe.ts
@@ -6,7 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class DeviceType implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    console.log("from device type pipe", value)
     return value=='WBS' ? 'assets/icons/whatsB.svg': value=='SMS'? 'assets/icons/sms.svg' :'assets/icons/whats.svg'
 
    
