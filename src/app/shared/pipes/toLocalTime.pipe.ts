fc8448b0d2d9a5657e326372639ad71e44090317
Ahlam Ragab <ahlam.ragab2000@gmail.com>
2023-12-20 00:20:56 +0200

    focus color change , edit date pipe , translate role

--- src/app/shared/pipes/toLocalTime.pipe.ts
+++ src/app/shared/pipes/toLocalTime.pipe.ts
@@ -9,18 +9,28 @@ export class ToLocalTimePipe implements PipeTransform {
     if (utcDate) {
       utcDate = utcDate.indexOf('Z') > -1 ? utcDate : (utcDate + 'Z');
       const localDate = new Date(utcDate);
-      
+
       const day = localDate.getDate();
       const month = localDate.getMonth() + 1; // Months are 0-indexed, so add 1
       const year = localDate.getFullYear();
-      const hour = localDate.getHours();
-      const minute = localDate.getMinutes();
-  
+      let hour = localDate.getHours();
+      let minute = localDate.getMinutes();
+
+      // Convert hours to 12-hour format and determine AM/PM
+      const amPm = hour >= 12 ? 'PM' : 'AM';
+      hour = hour % 12 || 12; // Handle 0 as 12 for 12-hour format
+
+      // Add leading '0' if the hour is less than 10
+      const formattedHour = hour < 10 ? '0' + hour : hour;
+
+      // Add leading '0' if the minute is less than 10
+      const formattedMinute = minute < 10 ? '0' + minute : minute;
+
       // Create a formatted date string
-      const formattedDate = `${day}/${month}/${year}, ${hour}:${minute}`;
-  
+      const formattedDate = `${day}/${month}/${year}, ${formattedHour}:${formattedMinute} ${amPm}`;
+
       return formattedDate;
-    }
+  }
   }
 
 }
